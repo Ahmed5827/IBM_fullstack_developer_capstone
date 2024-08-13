22a835005963c62cf657f07095114e1517e238f4
Ahmed5827 <ssjzchebbi@gmail.com>
2024-08-13 09:49:35 -0400

    added CarModel and CarMake

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,16 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://achebbi2002-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://achebbi2002-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'achebbi2002-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    # Add other hosts as needed
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://achebbi2002-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    # Add other origins as needed
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
